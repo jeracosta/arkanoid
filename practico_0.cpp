348df71d15a5fb4fd29fd971ed60f79b2b97f79a
jismaac <jeronimo2613@gmail.com>
2026-03-03 11:09:29 -0300

    style(practico_0.cpp): fix indentation

--- practico_0.cpp
+++ practico_0.cpp
@@ -1,38 +1,49 @@
 #include <optional>
 
-class coord
-{
-  public:
-    float x, y, z;
+class coord {
+public:
+  float x, y, z;
 
-  coord(float x, float y, float z)
-    : x(x), y(y), z(z)
-  {
-  }
+  coord(float x, float y, float z) : x(x), y(y), z(z) {}
 
   float length() {
-      if (!_length_cache)
-          _length_cache = _compute_length();
-      return *_length_cache;
-  } 
+    if (!_length_cache)
+      _length_cache = _compute_length();
+    return *_length_cache;
+  }
 
-  float length() const {
-      return _compute_length();
-  } 
+  float length() const { return _compute_length(); }
 
   float sqrlength();
-  coord operator +(const coord &other) { return coord(x + other.x, y + other.y, z + other.z); }
-  coord operator -(const coord &other) { return coord(x - other.x, y - other.y, z - other.z); }
-  coord operator *(float scale) { return coord(x*scale, y*scale, z*scale); }
-  coord operator /(float scale);
-  coord dot_product (const coord &other);
-  coord vec_product (const coord &other);
-  coord angle (const coord &other);
+
+  coord operator+(const coord &other) {
+    return coord(x + other.x, y + other.y, z + other.z);
+  }
+
+  coord operator-(const coord &other) {
+    return coord(x - other.x, y - other.y, z - other.z);
+  }
+
+  coord operator*(float scale) {
+    return coord(x * scale, y * scale, z * scale);
+  }
+
+  coord operator/(float scale);
+
+  coord dot_product(const coord &other);
+
+  coord vec_product(const coord &other);
+
+  coord angle(const coord &other);
+
   coord division(const coord &other);
-  coord operator -(); //negate vector
+
+  // negate vector
+  coord operator-();
+
   coord normalize();
- 
-  private:
-    std::optional<float> _length_cache = {};
-    float _compute_length() const;
+
+private:
+  std::optional<float> _length_cache = {};
+  float _compute_length() const;
 };

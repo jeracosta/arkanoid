c5b2e231cd803098c8c2d24d759aa498dd83a60b
jismaac <jeronimo2613@gmail.com>
2026-03-04 17:13:18 -0300

    style(vec3): rename vec3 => Vector3
    Es más comprensible al usar PascalCase (distingue mejor tipos) y evitar acortar nombre.
    Así le llaman a este tipo en Vulkan btw.

--- src/Vector3.hpp
+++ src/Vector3.hpp
@@ -1,13 +1,13 @@
 #include <cmath>
 #include <array>
 
-class vec3 
+class Vector3
 {
 private:
   std::array<float, 3> _coords;
 
 public:
-  constexpr vec3(float x, float y, float z)
+  constexpr Vector3(float x, float y, float z)
     : _coords{x, y, z}
   {
   }
@@ -36,35 +36,35 @@ public:
   }
 };
 
-inline vec3 operator+(const vec3 &left, const vec3 &right)
+inline Vector3 operator+(const Vector3 &left, const Vector3 &right)
 {
-  return vec3(left[0] + right[0],
+  return Vector3(left[0] + right[0],
               left[1] + right[1],
               left[2] + right[2]);
 }
 
-inline vec3 operator-(const vec3 &left, const vec3 &right)
+inline Vector3 operator-(const Vector3 &left, const Vector3 &right)
 {
-  return vec3(left[0] - right[0],
+  return Vector3(left[0] - right[0],
               left[1] - right[1],
               left[2] - right[2]);
 }
 
-inline vec3 operator*(const vec3 &vector, float scale)
+inline Vector3 operator*(const Vector3 &vector, float scale)
 {
-  return vec3(vector[0] * scale,
+  return Vector3(vector[0] * scale,
               vector[1] * scale,
               vector[2] * scale);
 }
 
-inline vec3 operator/(const vec3 &vector, float scale)
+inline Vector3 operator/(const Vector3 &vector, float scale)
 {
-  return vec3(vector[0] / scale,
+  return Vector3(vector[0] / scale,
               vector[1] / scale,
               vector[2] / scale);
 }
 
-inline float dot_product(const vec3 &left, const vec3 &right) 
+inline float dot_product(const Vector3 &left, const Vector3 &right) 
 {
   return
     left[0] * right[0] +
@@ -72,24 +72,24 @@ inline float dot_product(const vec3 &left, const vec3 &right)
     left[2] * right[2];
 }
 
-inline vec3 cross_product(const vec3 &left, const vec3 &right)
+inline Vector3 cross_product(const Vector3 &left, const Vector3 &right)
 {
-  return vec3(left[1] * right[2] - left[2] * right[1],
+  return Vector3(left[1] * right[2] - left[2] * right[1],
               left[2] * right[0] - left[0] * right[2],
               left[0] * right[1] - left[1] * right[0]);
 }
 
-inline float angle(const vec3 &left, const vec3 &right)
+inline float angle(const Vector3 &left, const Vector3 &right)
 {
   return dot_product(left, right) / (left.length() * right.length());
 }
 
-inline vec3 normal(const vec3 &vector)
+inline Vector3 normal(const Vector3 &vector)
 {
   return vector / vector.length();
 }
 
-inline vec3 operator-(const vec3 &vector)
+inline Vector3 operator-(const Vector3 &vector)
 {
-  return vec3(0,0,0) - vector;
+  return Vector3(0,0,0) - vector;
 }

7800e93fea83fba273e52430fad4123648313a80
jismaac <jeronimo2613@gmail.com>
2026-03-17 02:03:58 -0300

    feat: add application.hpp
    Abstrae boilerplate e impone estructura sobre la aplicación

---
+++ src/application.hpp
@@ -0,0 +1,101 @@
+#include "chronometer.hpp"
+#include <GL/gl.h>
+#include <SDL2/SDL.h>
+#include <glm/gtc/noise.hpp>
+#include <ratio>
+
+class Application
+{
+  public:
+    using TimeUnit = std::chrono::duration<float, std::ratio<1>>;
+
+    struct ExcecutionContext
+    {
+        const Chronometer<TimeUnit>::Reading &time;
+
+        // Triggers a graceful shutdown of the application at the end of the current frame.
+        std::function<void()> stop;
+    };
+
+    struct Configuration
+    {
+        struct
+        {
+            const char *title;
+            glm::uvec2  size;
+        } window;
+
+        std::function<void(const ExcecutionContext &, const SDL_Event &)> event_logic;
+
+        std::function<void(const ExcecutionContext &)> frame_logic;
+    };
+
+  private:
+    SDL_Window   *window_;
+    SDL_GLContext gl_context_;
+    bool          running_;
+    Configuration config_;
+
+  public:
+    Application(Configuration config)
+        : config_(std::move(config))
+    {
+        if (SDL_Init(SDL_INIT_VIDEO))
+        {
+            fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
+            std::exit(EXIT_FAILURE);
+        }
+
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, true);
+    }
+
+    Application(const Application &) = delete;
+
+    ~Application()
+    {
+        SDL_Quit();
+    }
+
+    void
+    run()
+    {
+        window_ = SDL_CreateWindow(config_.window.title,
+                                   SDL_WINDOWPOS_CENTERED,
+                                   SDL_WINDOWPOS_CENTERED,
+                                   config_.window.size.x,
+                                   config_.window.size.y,
+                                   SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+        gl_context_ = SDL_GL_CreateContext(window_);
+
+        auto chronometer = Chronometer<TimeUnit>{};
+
+        running_ = true;
+
+        while (running_)
+        {
+            auto context = ExcecutionContext{
+                .time = chronometer.read(),
+                .stop = [this]() { running_ = false; },
+            };
+
+            auto event = SDL_Event{};
+            while (SDL_PollEvent(&event))
+            {
+                if (event.type == SDL_QUIT)
+                {
+                    context.stop();
+                }
+
+                config_.event_logic(context, event);
+            }
+
+            config_.frame_logic(context);
+
+            SDL_GL_SwapWindow(window_);
+        }
+
+        SDL_GL_DeleteContext(gl_context_);
+        SDL_DestroyWindow(window_);
+    }
+};

a935cfbca3cd0761531ecac4f3f93f5047f9b94a
jismaac <jeronimo2613@gmail.com>
2026-03-17 02:03:25 -0300

    feat: add chronometer.hpp
    Permite medir tiempo de manera cómoda

---
+++ src/chronometer.hpp
@@ -0,0 +1,45 @@
+#include <chrono>
+
+template <typename Duration>
+class Chronometer
+{
+  private:
+    using Clock = std::chrono::steady_clock;
+
+    Clock::time_point start_time_ = Clock::now();
+    Clock::time_point last_time_  = start_time_;
+
+  public:
+    void
+    reset()
+    {
+        start_time_ = Clock::now();
+        last_time_  = start_time_;
+    }
+
+    struct Reading
+    {
+        Duration::rep elapsed; // Time since the chronometer was started
+        Duration::rep delta;   // Time since the last reading
+    };
+
+    Reading
+    read()
+    {
+        auto now = Clock::now();
+
+        auto countSince = [&now](Clock::time_point time)
+        {
+            return std::chrono::duration_cast<Duration>(now - time).count();
+        };
+
+        auto reading = Reading{
+            .elapsed = countSince(start_time_),
+            .delta   = countSince(last_time_),
+        };
+
+        last_time_ = now;
+
+        return reading;
+    }
+};

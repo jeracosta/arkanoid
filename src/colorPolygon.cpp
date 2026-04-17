beb2f1a15adc4836903dc9ef16a801ec2824d6b1
jismaac <jeronimo2613@gmail.com>
2026-04-17 16:26:05 -0300

    fix(colorPolygon): adapt to last Application class refactor

--- src/colorPolygon.cpp
+++ src/colorPolygon.cpp
@@ -1,53 +1,31 @@
-#include "application.hpp"
+#include "game.hpp"
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <SDL2/SDL.h>
-#include <cmath>
 #include <cstdlib>
 #include <print>
-
-auto
-frame(const Application::RuntimeContext &frame)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClear(GL_DEPTH_BUFFER_BIT);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glBegin(GL_TRIANGLES);
-    {
-        glColor3f(1.0, 0.0, 0.0); // Rojo
-        glVertex3f(-1.5, 1.0, -6.0);
-        glColor3f(0.0, 1.0, 0.0); // Verde
-        glVertex3f(-2.5, -1.0, -6.0);
-        glColor3f(0.0, 0.0, 1.0); // Azul
-        glVertex3f(-0.5, -1.0, -6.0);
-    }
-    glEnd();
-}
 
 int
 main()
 {
-    auto app = Application({
+    Game::run({
       .window = {
-          .title = "Test SDL app",
-          .size  = {640, 480},
+            .title = "Test SDL app",
+            .size  = {640, 480},
       },
 
-      .input_setup = [](auto &inputs, auto &context)
+      .configure_input = [](auto &inputs, auto &game)
       {
-          inputs.bind(SDLK_ESCAPE, KeyInput::Release, InputAction{context.stop});
+          inputs.bind(SDLK_ESCAPE, KeyInput::Release, [&]{ game.stop(); });
 
           inputs.bind(SDLK_F10, KeyInput::Press, [&]{
-              auto size = context.window.size();
+              auto size = game.window.size();
               std::println("Toggling fullscreen mode. Current size: {} x {}", size.x, size.y);
-              context.window.toggle_fullscreen();
+              game.window.toggle_fullscreen();
           });
       },
 
-      .init = []
+      .on_init = []
       {
           glClearColor(0.0,0.0,0.0,1.0); // Negro
           glMatrixMode(GL_PROJECTION);
@@ -55,11 +33,26 @@ main()
           gluPerspective(45, (640.0 / 480.0), 0.1, 100);
       },
 
-      .frame_logic = frame,
-
+      .on_update = [](auto &)
+      {
+          glClear(GL_COLOR_BUFFER_BIT);
+          glClear(GL_DEPTH_BUFFER_BIT);
+      
+          glMatrixMode(GL_MODELVIEW);
+          glLoadIdentity();
+      
+          glBegin(GL_TRIANGLES);
+          {
+              glColor3f(1.0, 0.0, 0.0); // Rojo
+              glVertex3f(-1.5, 1.0, -6.0);
+              glColor3f(0.0, 1.0, 0.0); // Verde
+              glVertex3f(-2.5, -1.0, -6.0);
+              glColor3f(0.0, 0.0, 1.0); // Azul
+              glVertex3f(-0.5, -1.0, -6.0);
+          }
+          glEnd();
+      }
     });
-
-    app.run();
 
     return EXIT_SUCCESS;
 }

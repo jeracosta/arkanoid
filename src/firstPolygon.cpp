a8f70e66617a125ccbac928688beefe5f5e52db0
jismaac <jeronimo2613@gmail.com>
2026-04-17 16:54:07 -0300

    fix(firstPolygon): adapt to last Application class refactor

--- src/firstPolygon.cpp
+++ src/firstPolygon.cpp
@@ -1,63 +1,25 @@
-#include "application.hpp"
+#include "game.hpp"
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <SDL2/SDL.h>
 #include <cmath>
 #include <cstdlib>
-#include <print>
-
-auto
-frame(const Application::RuntimeContext &frame)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClear(GL_DEPTH_BUFFER_BIT);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glBegin(GL_TRIANGLES);
-    {
-        glColor3f(1.0, 0.0, 0.0); // Rojo
-        glVertex3f(-1.5, 1.0, -6.0);
-        glColor3f(0.0, 1.0, 0.0); // Verde
-        glVertex3f(-2.5, -1.0, -6.0);
-        glColor3f(0.0, 0.0, 1.0); // Azul
-        glVertex3f(-0.5, -1.0, -6.0);
-    }
-    glEnd();
-
-    glTranslatef(-1.5f, 0.0f, -sin(frame.time.elapsed) / 0.01);
-
-    glBegin(GL_QUADS);
-    {
-        glColor3f(sin(frame.time.elapsed) / 2 + 0.5, 1.0, 0.0);
-
-        glVertex3f(0.5, 1.0, -6.0);
-        glVertex3f(2.5, 1.0, -6.0);
-
-        glColor3f(1.0, 1.0, sin(frame.time.elapsed) / 2 + 0.5);
-
-        glVertex3f(2.5, -1.0, -6.0);
-        glVertex3f(0.5, -1.0, -6.0);
-    }
-    glEnd();
-}
 
 int
 main()
 {
-    auto app = Application({
+    Game::run({
       .window = {
           .title = "Test SDL app",
           .size  = {640, 480},
       },
 
-      .input_setup = [](auto &inputs, auto &context)
+      .configure_input = [](auto &inputs, auto &game)
       {
-          inputs.bind(SDLK_ESCAPE, KeyInput::Release, InputAction{context.stop});
+          inputs.bind(SDLK_ESCAPE, KeyInput::Release, [&]{ game.stop(); });
       },
 
-      .init = []
+      .on_init = []
       {
           glClearColor(0.0,0.0,0.0,1.0); // Negro
           glMatrixMode(GL_PROJECTION);
@@ -65,11 +27,43 @@ main()
           gluPerspective(45, (640.0 / 480.0), 0.1, 100);
       },
 
-      .frame_logic = frame,
+      .on_update = [](auto &game)
+      {
+          glClear(GL_COLOR_BUFFER_BIT);
+          glClear(GL_DEPTH_BUFFER_BIT);
+      
+          glMatrixMode(GL_MODELVIEW);
+          glLoadIdentity();
+      
+          glBegin(GL_TRIANGLES);
+          {
+              glColor3f(1.0, 0.0, 0.0); // Rojo
+              glVertex3f(-1.5, 1.0, -6.0);
+              glColor3f(0.0, 1.0, 0.0); // Verde
+              glVertex3f(-2.5, -1.0, -6.0);
+              glColor3f(0.0, 0.0, 1.0); // Azul
+              glVertex3f(-0.5, -1.0, -6.0);
+          }
+          glEnd();
+      
+          glTranslatef(-1.5f, 0.0f, -sin(game.time.elapsed()) / 0.01);
+      
+          glBegin(GL_QUADS);
+          {
+              glColor3f(sin(game.time.elapsed()) / 2 + 0.5, 1.0, 0.0);
+      
+              glVertex3f(0.5, 1.0, -6.0);
+              glVertex3f(2.5, 1.0, -6.0);
+      
+              glColor3f(1.0, 1.0, sin(game.time.elapsed()) / 2 + 0.5);
+      
+              glVertex3f(2.5, -1.0, -6.0);
+              glVertex3f(0.5, -1.0, -6.0);
+          }
+          glEnd();
+      }
 
     });
-
-    app.run();
 
     return EXIT_SUCCESS;
 }

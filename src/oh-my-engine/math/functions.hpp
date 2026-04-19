f5503b8c8124e400385615b1623159283bb207a0
jismaac <jeronimo2613@gmail.com>
2026-04-19 05:42:06 -0300

    feat(ome::math): add make_sigmoid support for Vectors

--- src/oh-my-engine/math/functions.hpp
+++ src/oh-my-engine/math/functions.hpp
@@ -13,17 +13,21 @@ make_lerp(float from, float to)
     return [=](float t) { return std::lerp(from, to, t); };
 };
 
-template <is_vector Vector>
-inline auto
-make_lerp(const Vector &from, const Vector &to)
-{
-    return transform(make_lerp, from, to);
-}
-
 inline auto
 make_sigmoid(float steepness = 1.0f, float midpoint = 0.0f)
 {
     return [=](float t) { return 2.0f / (1.0f + std::exp(-steepness * (t - midpoint))); };
 }
+
+#define DEFINE_VECTOR_OVERLOAD(FUNC)                                                               \
+    template <is_vector Vector>                                                                    \
+    inline auto FUNC(const Vector &from, const Vector &to)                                         \
+    {                                                                                              \
+        return [=](float t)                                                                        \
+        { return transform([t](auto a, auto b) { return FUNC(a, b)(t); }, from, to); };            \
+    }
+DEFINE_VECTOR_OVERLOAD(make_lerp)
+DEFINE_VECTOR_OVERLOAD(make_sigmoid)
+#undef DEFINE_VECTOR_OVERLOAD
 
 } // namespace ome::math

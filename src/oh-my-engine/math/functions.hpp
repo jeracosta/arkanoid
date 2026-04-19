1d01a84b07e31f8da788eabcf3643c8545d9c054
jismaac <jeronimo2613@gmail.com>
2026-04-18 23:12:51 -0300

    feat(ome::math): add make_sigmoid

--- src/oh-my-engine/math/functions.hpp
+++ src/oh-my-engine/math/functions.hpp
@@ -11,4 +11,10 @@ make_lerp(float from, float to)
     return [=](float t) { return std::lerp(from, to, t); };
 };
 
+inline auto
+make_sigmoid(float steepness = 1.0f, float midpoint = 0.0f)
+{
+    return [=](float t) { return 2.0f / (1.0f + std::exp(-steepness * (t - midpoint))); };
+}
+
 } // namespace ome::math

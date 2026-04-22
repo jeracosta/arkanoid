43cfbd0b4d106edae5e73535b130f00b24dd8849
jismaac <jeronimo2613@gmail.com>
2026-04-22 00:08:27 -0300

    feat(ECS): add HitSphere component

---
+++ src/oh-my-engine/entity-component-system/components/hit_sphere.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "oh-my-engine/math/sphere.hpp"
+#include "oh-my-engine/math/vector.hpp"
+
+namespace ome::ecs::components {
+
+struct HitSphere
+{
+    float radius;
+};
+
+} // namespace ome::ecs::components

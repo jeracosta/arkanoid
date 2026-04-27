aae563a3e3ddbc11d62432098d55f612d27acfe1
jismaac <jeronimo2613@gmail.com>
2026-04-27 00:36:16 -0300

    refactor(events): simplification + flexibilization

--- src/oh-my-engine/events.hpp
+++ src/oh-my-engine/events.hpp
@@ -3,118 +3,95 @@
 #include <boost/mp11.hpp>
 #include <functional>
 #include <memory>
-#include <tuple>
 #include <type_traits>
 #include <vector>
 
 namespace ome {
 
-template <class TTypes>
-struct events_specification
-{
-    using Tuple = std::tuple<>;
+template <class... TEvents>
+class EventDispatcher; // forward declaration
 
-    template <class TEvent>
-    static constexpr bool is_event = false;
+// Macro to be used inside a class definition to define the event types it can emit.
+#define DEFINE_EVENTS(...) using Events = ::ome::events_specification<__VA_ARGS__>
+
+struct EventConnection
+{
+  private:
+    // Parameter type-erased to support different event types while
+    // allowing heterogeneous storage of connections.
+    std::function<void(void *event)> callback_;
+
+    template <class... TEvents>
+    friend class EventDispatcher;
 };
 
-template <class TTypes>
-    requires requires { std::tuple_size<TTypes>::value; }
-struct events_specification<TTypes>
-{
-    using Tuple = TTypes;
-
-    template <class TEvent>
-    static constexpr bool is_event = boost::mp11::mp_contains<Tuple, TEvent>::value;
-
-    template <class TEvent>
-        requires is_event<TEvent>
-    static constexpr std::size_t code_of = boost::mp11::mp_find<Tuple, TEvent>::value;
-};
-
-#define DEFINE_EVENTS(...) using Events = ::ome::events_specification<std::tuple<__VA_ARGS__>>
-
-template <class TEmitter, class TEvent, class TCallable>
-static constexpr bool handles_event = TEmitter::Events::template is_event<TEvent>
-                                      && std::is_invocable_v<TCallable, TEmitter &, const TEvent &>;
-
-template <class TEmitter, class TCallable>
-static constexpr bool handles_any_event = std::apply([](auto... events) constexpr
-{ return (std::is_invocable_v<TCallable, TEmitter &, decltype(events)> || ...); },
-                                                     typename TEmitter::Events::Tuple{});
-
-// Component of a class (TEmitter) that can dispatch events to registered callbacks.
-template <class TEmitter>
-    requires requires { typename TEmitter::Events; }
+template <class... TEvents>
 class EventDispatcher
 {
   private:
-    TEmitter &emitter_;
+    using Events_ = boost::mp11::mp_list<TEvents...>;
 
-    enum class CallbackResponse_
+    template <class TEvent>
+    using ConnectionVector_ = std::vector<std::weak_ptr<EventConnection>>;
+
+    using ConnectionMatrix_
+        = boost::mp11::mp_rename<boost::mp11::mp_transform<ConnectionVector_, Events_>, std::tuple>;
+
+    ConnectionMatrix_ callback_matrix_;
+
+    template <class TEvent>
+    ConnectionVector_<TEvent> &
+    connections_()
     {
-        Nil = 0, // default value representing no response
-        Unbind,  // callback requested to stop being called
-    };
-
-    template <class TEvent>
-    using Callback_ = std::function<CallbackResponse_(TEmitter &, const TEvent &)>;
-
-    template <class TEvent>
-    using CallbackVector_ = std::vector<Callback_<TEvent>>;
-
-    using CallbackMatrix_
-        = boost::mp11::mp_transform<CallbackVector_, typename TEmitter::Events::Tuple>;
-
-    CallbackMatrix_ callback_matrix_;
-
-    template <class TEvent>
-    auto &
-    callbacks_for_()
-    {
-        return std::get<TEvent>(callback_matrix_);
+        constexpr auto index = boost::mp11::mp_find<Events_, TEvent>::value;
+        return std::get<index>(callback_matrix_);
     }
+
+    template <class TEvent>
+    static constexpr bool supported_ = boost::mp11::mp_contains<Events_, TEvent>::value;
 
   public:
-    template <class TEvent, class TObserver, class TCallable>
-    void
-    bind(TCallable TObserver::*method, std::weak_ptr<TObserver> observer)
+    // Registers a callback for an event and returns a handle to the connection.
+    // The callback is invoked whenever the event is emitted, receiving the event as an argument.
+    // The subscription remains active while the returned connection handle is alive.
+    template <class TEvent, class TCallable>
+    [[nodiscard]] std::shared_ptr<EventConnection>
+    bind(TCallable &&callback)
     {
-        auto callback
-            = [method, observer](TEmitter &emitter, const TEvent &event) -> CallbackResponse_
-        {
-            if (auto obs = observer.lock())
-            {
-                (obs.get()->*method)(emitter, event);
-                return CallbackResponse_::Nil;
-            }
-            else
-            {
-                return CallbackResponse_::Unbind;
-            }
-        };
+        static_assert(supported_<TEvent>,
+                      "Tried binding to an event that is not supported by the dispatcher.");
 
-        static_assert(handles_event<TEmitter, TEvent, decltype(callback)>);
+        static_assert(std::is_invocable_v<TCallable, const TEvent &>,
+                      "Tried binding a callback that cannot be invoked with the event type.");
 
-        callbacks_for_<TEvent>().emplace_back(std::move(callback));
+        auto callback_wrapper = [callback = std::forward<TCallable>(callback)](void *event)
+        { callback(*static_cast<const TEvent *>(event)); };
+
+        auto connection       = std::make_shared<EventConnection>();
+        connection->callback_ = std::move(callback_wrapper);
+
+        connections_<TEvent>().push_back(connection);
+
+        return connection;
     }
 
     template <class TEvent>
-        requires TEmitter::Events::template
-    is_event<TEvent> void
-    dispatch(const TEvent &event)
+        requires supported_<TEvent>
+    void
+    emit(const TEvent &event)
     {
-        auto &callbacks = callbacks_for_<TEvent>();
+        auto &connections = connections_<TEvent>();
 
-        for (auto it = callbacks.begin(); it != callbacks.end();)
+        for (auto it = connections.begin(); it != connections.end();)
         {
-            if ((*it)(emitter_, event) == CallbackResponse_::Unbind)
+            if (auto connection = it->lock())
             {
-                it = callbacks.erase(it);
+                std::invoke(connection->callback_, const_cast<TEvent *>(&event));
+                ++it;
             }
             else
             {
-                ++it;
+                it = connections.erase(it);
             }
         }
     }

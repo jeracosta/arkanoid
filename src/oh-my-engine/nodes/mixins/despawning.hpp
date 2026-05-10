7d2cb903e05c1233ef90f197dd54de2954dcef10
jismaac <jeronimo2613@gmail.com>
2026-05-10 11:21:00 -0300

    feat(ome nodes): add Despawning mixin

---
+++ src/oh-my-engine/nodes/mixins/despawning.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "oh-my-engine/node.hpp"
+
+namespace ome {
+
+// Adapts a node to provide despawning functionality: it will die when a certain condition is met.
+template <std::derived_from<ome::Node> TBase, typename TShouldDespawn>
+class Despawning : public TBase
+{
+  public:
+    void
+    on_tick_() override
+    {
+        [[unlikely]]
+        if (TShouldDespawn{}(*this))
+        {
+            this->schedule_unmount();
+        }
+
+        TBase::on_tick_();
+    }
+};
+
+} // namespace ome

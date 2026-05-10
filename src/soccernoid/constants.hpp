e4ef4f8bb1c832f6e3446483a59a3b5e6cda8837
jismaac <jeronimo2613@gmail.com>
2026-05-10 11:21:29 -0300

    feat(soccernoid nodes): add DistanceCulled mixin

---
+++ src/soccernoid/constants.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace soccernoid {
+
+static constexpr float despawn_distance = 5.0f;
+
+} // namespace soccernoid
---
+++ src/soccernoid/nodes/mixins/distance_culled.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "oh-my-engine/nodes/mixins/despawning.hpp"
+#include "oh-my-engine/nodes/transform_node.hpp"
+#include "soccernoid/constants.hpp"
+
+namespace soccernoid {
+
+inline constexpr auto exceeds_despawn_distance = [](const ome::TransformNode &node)
+{ return norm(node.world_transform().position) > despawn_distance; };
+
+template <std::derived_from<ome::Node> TBase>
+using DistanceCulled = ome::Despawning<TBase, decltype(exceeds_despawn_distance)>;
+
+} // namespace soccernoid

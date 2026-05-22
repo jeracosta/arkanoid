04e36d8dc230c2e7c5df7e80e8ff858114f50c61
jismaac <jeronimo2613@gmail.com>
2026-05-22 17:48:34 -0300

    feat(TeapotNode): spring scale on projectile collision

--- src/soccernoid/nodes/teapot.hpp
+++ src/soccernoid/nodes/teapot.hpp
@@ -1,32 +1,81 @@
 #pragma once
 
 #include <cmath>
+#include <memory>
 
 #include "oh-my-engine/nodes/hitbox_node.hpp"
 #include "oh-my-engine/nodes/mesh_node.hpp"
 #include "oh-my-engine/nodes/transform_node.hpp"
+#include "oh-my-engine/spline.hpp"
+#include "oh-my-engine/spring_curve.hpp"
 #include "soccernoid/constants.hpp"
+#include "soccernoid/nodes/projectile.hpp"
 
 namespace soccernoid {
 
 class TeapotNode : public ome::TransformNode
 {
+    class HitboxNode_ : public ome::HitboxNode
+    {
+      public:
+        using ome::HitboxNode::HitboxNode;
+
+        TeapotNode *
+        teapot()
+        {
+            return static_cast<TeapotNode *>(parent()->parent());
+        }
+
+      protected:
+        void
+        on_collision_(ome::HitboxNode &other) override
+        {
+            if (dynamic_cast<ProjectileNode *>(other.parent()))
+            {
+                teapot()->process_->restart();
+            }
+        }
+    };
+
+    std::unique_ptr<ome::CurveProcess<float>> process_;
+    ome::MeshNode                            *mesh_node_ = nullptr;
+    ome::Vec3f                                mesh_size_ = {};
+
   public:
     TeapotNode()
     {
-        auto mesh = static_cast<std::shared_ptr<ome::Mesh>>(meshes.teapot);
+        auto mesh  = static_cast<std::shared_ptr<ome::Mesh>>(meshes.teapot);
         mesh_size_ = mesh->size();
 
+        auto envelope = std::make_shared<ome::Spline<float>>(ome::Spline<float>::catmull_rom({
+            { 0.0f, 0.0f },
+            { 0.5f, 1.0f },
+            { 1.0f, 0.0f },
+        }));
+
+        auto spring         = std::make_shared<ome::SpringCurve>();
+        spring->envelope_ = std::move(envelope);
+        process_          = std::make_unique<ome::CurveProcess<float>>(spring, 1.5f);
+
         auto &node = emplace_child<ome::MeshNode>(mesh, ome::Material{ .shininess = 25.0f });
-        node.emplace_child<ome::HitboxNode>(mesh_size_, mesh->center());
+        node.emplace_child<HitboxNode_>(mesh_size_, mesh->center());
 
         mesh_node_ = &node;
     }
 
     void
+    on_mount_() override
+    {
+        process_->complete();
+    }
+
+    void
     on_tick_() override
     {
+        process_->update(game()->time.delta());
+
         auto angle = game()->time.elapsed();
+        auto s     = process_->value();
 
         ome::Orientation rot;
         rot.rotate(angle, ome::up);
@@ -37,6 +86,7 @@ class TeapotNode : public ome::TransformNode
         {
             t.orientation = rot;
             t.position    = { 0.0f, vertical, 0.0f };
+            t.scale       = { s, s, s };
         });
 
         mesh_node_->update_material([&](auto &mat)
@@ -45,10 +95,6 @@ class TeapotNode : public ome::TransformNode
             mat.color.diffuse = mat.color.ambient;
         });
     }
-
-  private:
-    ome::MeshNode *mesh_node_ = nullptr;
-    ome::Vec3f     mesh_size_ = {};
 };
 
 } // namespace soccernoid

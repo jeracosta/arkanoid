362fa0258fd6d949ced5557de5570fd067209e56
Elim Pizza <elimpizza@gmail.com>
2026-05-24 14:44:26 -0300

    fix(Texture): dont do incorrect flipping

--- src/oh-my-engine/texture.cpp
+++ src/oh-my-engine/texture.cpp
@@ -2,7 +2,6 @@
 
 #include <GL/glu.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <cstring>
 #include <ranges>
@@ -54,34 +53,10 @@ Texture::load(const std::filesystem::path &path)
             std::format("Failed to convert texture `{}`: {}", path.string(), SDL_GetError()));
     }
 
-    auto size             = Vec2u{ surface->w, surface->h };
-    auto &[width, height] = size;
-
+    auto size  = Vec2u{ surface->w, surface->h };
     auto image = Primitive(size);
 
-    // NOTE: SDL surfaces are stored in top-down order, but OpenGL expects bottom-up order.
-    //       We will copy the rows in reverse order to effectively flip the image vertically.
-
-    using namespace std::views;
-
-    auto rows = [height](void *storage, std::size_t pitch)
-    {
-        auto *base = static_cast<std::byte *>(storage);
-
-        return iota(std::size_t{ 0 }, static_cast<std::size_t>(height))
-               | transform([=](std::size_t i)
-        { return reinterpret_cast<Primitive::Pixel *>(base + i * pitch); });
-    };
-
-    auto row_size = width * sizeof(Primitive::Pixel);
-
-    auto image_rows   = rows(image.raw(), row_size);
-    auto surface_rows = rows(surface->pixels, surface->pitch);
-
-    for (auto &&[image_row, surface_row] : zip(image_rows, surface_rows | reverse))
-    {
-        std::memcpy(image_row, surface_row, row_size);
-    }
+    std::memcpy(image.raw(), surface->pixels, size[0] * size[1] * sizeof(Primitive::Pixel));
 
     SDL_FreeSurface(surface);
 

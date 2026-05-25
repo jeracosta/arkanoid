45c1f978991efa3494ab49293fd2b2dc89dd92f1
Elim Pizza <elimpizza@gmail.com>
2026-05-25 00:10:14 -0300

    chore(VictoryScreen): translate to spanish

--- src/soccernoid/nodes/victory_screen.hpp
+++ src/soccernoid/nodes/victory_screen.hpp
@@ -14,22 +14,17 @@ class VictoryScreenNode : public SoccernoidNode<>
     {
         const auto *viewport = ImGui::GetMainViewport();
 
-        ImGui::SetNextWindowPos(viewport->GetCenter(),
-                                ImGuiCond_Always,
-                                ImVec2{ 0.5f, 0.5f });
+        ImGui::SetNextWindowPos(viewport->GetCenter(), ImGuiCond_Always, ImVec2{ 0.5f, 0.5f });
 
-        constexpr auto flags = ImGuiWindowFlags_NoDecoration
-                               | ImGuiWindowFlags_AlwaysAutoResize
-                               | ImGuiWindowFlags_NoMove
-                               | ImGuiWindowFlags_NoSavedSettings
-                               | ImGuiWindowFlags_NoFocusOnAppearing
-                               | ImGuiWindowFlags_NoInputs;
+        constexpr auto flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize
+                               | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings
+                               | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoInputs;
 
         ImGui::Begin("##victory-screen", nullptr, flags);
 
         ImGui::PushStyleColor(ImGuiCol_Text, ImVec4{ 0.15f, 1.0f, 0.3f, 1.0f });
         ImGui::SetWindowFontScale(4.0f);
-        ImGui::Text("VICTORY!");
+        ImGui::Text("¡VICTORIA! :)");
         ImGui::SetWindowFontScale(1.0f);
         ImGui::PopStyleColor();
 
